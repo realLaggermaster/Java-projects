5f2ab620105239caa2b7502422f351d8bbe36b7d
Laggermaster <contact.1f2d@gmail.com>
2025-01-03 23:13:07 +0200

    Added new java files

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+// 3. January 2025
 
 public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Integers
+        byte numInByte = 127;
+        short numInShort = 32767;
+        int numInInt = 2147483647;
+        long numInLong = 9223372036854775807L;
+        // Compute the distance light travels using long.
+        int lightSpeed = 186000;
+        long days = 1000;
+        long seconds = days * 24 * 60 *60;
+        long distance = lightSpeed + seconds;
+        System.out.println("In " + days + " days, the light travels " + distance + " kilometers.");
+
+        // Floating-point numbers
+        float myBankAccount = 12.99f; // f forces it to be float
+        double yourChanceToGetBitches = 0.00700001;
+        System.out.println(myBankAccount);
+        System.out.println(yourChanceToGetBitches);
+        // Area of a circle
+        double pi, r ,a;
+        r = 8.7;
+        pi = 3.1416;
+        a = pi * r * r;
+        System.out.println("Area of the circle is " + a);
+
+        // Characters
+        char letter = 937;
+        System.out.println(letter);
+        // Loop 1 - 50 unicodes
+        char unicodeChar = 1;
+        for (int i = 0; i < 50; i++) {
+            unicodeChar++;
+            System.out.println(unicodeChar);
+        }
+
+        // Booleans
+        boolean whiteGood = true;
+        boolean blackGood = false;
+        System.out.println(whiteGood);
+        System.out.println(blackGood);
+
+    }
+
 }
--- src/Variables_and_Printing.java
+++ src/Variables_and_Printing.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+// 3. January 2025
 
 public class Variables_and_Printing {
+    public static void main(String[] args) {
+
+        // Variables
+        String car = "BMW e36";
+        boolean working = true;
+        int carAge = 32;
+        double engineDisplacement = 2.5;
+
+        // Printing
+        System.out.println("Car: "+ car);
+        System.out.println("Working:"+ working);
+        System.out.println("Age:"+ carAge);
+        System.out.println("engineDisplacement"+ engineDisplacement);
+
+    }
+
 }
